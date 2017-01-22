518406ba78978d35225fb54018e4138cebe75384
semorris <semorris@ualberta.ca>
2017-01-22 11:12:14 -0700

    inital commit

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #  coding: utf-8 
-import SocketServer
+import SocketServer, os, mimetypes
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Sarah Morris
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,11 +28,89 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+
+        response = self.getResponse()
+        print "response: " + str(response)
+
+        self.request.sendall(response)
+
+    def getResponse(self):
+
+        # make sure the method of the request is a GET
+        if not self.check_method():
+            return self.bad_method()
+
+        # parse the path
+        return self.get_path()
+
+
+    def check_method(self):
+        method = self.data.split(' ', 1)
+        if method[0] != "GET":
+            return False
+        else:
+            return True
+
+    def bad_method(self):
+        status = "405 Method Not Allowed\r\n"
+        connection = "Connection: close\r\n"
+        return status
+
+    def get_path(self):
+        
+        # extract the path from the request
+        path = self.data.split(' ', 2)[1].strip("/")
+        
+        print "origional path: " + path + "\n"
+
+        # check if path has www already
+        if path.split("/", 1)[0] != 'www':
+            path = "www/" + path
+
+        path = os.path.abspath(path)
+        print("absolute path: " + path + "\n")
+
+        # check if the path is a directory or a file
+        if os.path.isdir(path):
+            print "path is a directory"
+            path = path + "/index.html"
+
+        # make sure the file exists
+        if os.path.isfile(path):
+            print "file exists"
+            return self.ok(path)
+        else:
+            return self.not_found()
+
+    def not_found(self):
+        # returns 404 not found response
+        status = "HTTP/1.1 404 Not Found\r\n"
+        contentType = "text/html\r\n"
+        connection = "Connection: close\r\n"
+
+        body = open("404.html", 'r').read()
+
+        return status + contentType + connection + "\r\n" + body
+
+    def ok(self, path):
+        # returns a 200 OK
+        status = "HTTP/1.1 200 OK\r\n"
+
+        # get the content length
+        fileSize = os.path.getsize(path)
+        contentLength = "Content-Length: " + str(fileSize) + "\r\n"
+
+        contentType = "Content-Type: " + mimetypes.guess_type(path)[0] + "\r\n"
+
+        connection = "Connection: close \r\n"
+
+        body = open(path, 'r').read()
+
+        return status + contentType + connection + "\r\n" + body + "\r\n"
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
